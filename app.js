854b6efbb3011692ef19016a9564e58b0656c31f
Hanane Benabid <benabid.hanane@gmail.com>
2025-06-30 14:35:46 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// fichier appel express et routes 
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req , res)=> {
+
+    res.send('Bonjour c est camille et Hanane');
+});
+
+app.listen(port, ()=>{
+
+    console.log(`Serveur adaopteBack sur http://localhost:${port}`);
+    
+});
