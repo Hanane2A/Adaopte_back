1115917737c7bd353024167b7b2cd0c8da2cb9b8
Camille-lt <camillelebigot5@gmail.com>
2025-07-07 12:16:47 +0200

    Mise à jour routes adoption : POST, PUT, DELETE fonctionnels

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3001;
 
 const animalRouter = require('./routes/animal'); // importe BDD animal
 const adopterRouter = require('./routes/adopter');
-
+const adoptionRouter = require('./routes/adoption');
 
 app.use(express.json()); // Middleware pour parser les requêtes JSON (très important pour les requêtes POST/PUT)
 
@@ -16,7 +16,7 @@ app.get('/', (req , res)=> {
 
 app.use('/animal', animalRouter);
 app.use('/adopter', adopterRouter);
-
+app.use('/adoption', adoptionRouter);
 
 app.listen(port, ()=>{
     console.log(`Serveur adaopteBack sur http://localhost:${port}`);
